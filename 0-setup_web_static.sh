3834715c859ed3f9c35d3a9f2f541a55ed6e6b23
Yermehyaw <yermehyaw@gmail.com>
2024-07-10 19:57:40 -0700

    Add tested sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -24,25 +24,16 @@ sudo chown -R "ubuntu":"ubuntu" /data/  # Give file and group ownership to user 
 
 # Edit server block to serve static web content in location /hbnb_static
 config_file="/etc/nginx/sites-enabled/$HOSTMANE.conf"
+# sudo -c "$config_file"  # In case config file hadnt bern created
+sudo sed -i "s/location \/ {/\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n\n\tlocation \/ {/g"  "$config_file"
+# single quotes can be used to prevent any unforseen shell interference on sed. Always ensure the root and alis direcrives ebd with a / followed by a semicolon.
+# an alias was used here mainly for security purposes inorder to obsure the underlying architecture of this page/resource, rather than usubg a root directive which is more revealing of the specific location where the resource is stored.
 
-sudo tee -a "$config_file" > /dev/null <<EOF  # write into the nginx default config file
-server {
-	listen 80;
-	server_name "$domain";
-
-	location / {
-		root "$webroot";
-		index index.html;
-		add_header X-Served-By "$HOSTNAME";
-       }
-}
-
-EOF
 
 # Activate server block if not already previously activated
 if [ -L "/etc/nginx/sites-enabled" ]  # Does sym link exists here
 then
-    sudo ln -s "$config" /etc/nginx/sites-enabled
+    sudo ln -s "$config" /etc/nginx/sites-enabled/
 fi
 
 # Reload/Start Nginx to apply changes
