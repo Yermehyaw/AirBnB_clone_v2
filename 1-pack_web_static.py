946e6e772daeede3900c68d9a1555f920e4c7eb4
Yermehyaw <yermehyaw@gmail.com>
2024-07-10 18:11:26 -0700

    Fix pyvodestyle errs

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,8 +5,8 @@
 Modules Imported: fabric.api, datetime
 
 fabric.api: Automate upload and download, connection and deployment of code.
-Also including remote and local shells interaction all via ssh. 
-fabric.api is an abstraction of the origonal fabric. 
+Also including remote and local shells interaction all via ssh.
+fabric.api is an abstraction of the origonal fabric.
 7It is fabric v1.14.post1 also known as fabric3
 
 datetime: Package conatins several modules to track and return time
