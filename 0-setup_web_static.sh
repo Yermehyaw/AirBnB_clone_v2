a3222dd5b895d1b669e6f9f913ca9e6979d05b58
Yermehyaw <yermehyaw@gmail.com>
2024-07-12 04:50:05 -0700

    Fix code structure

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,25 +15,24 @@ sudo ufw allow 'Nginx HTTP'
 # Create neccessary folders
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo touch -c  /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
 sudo chmod -R 755 /data/web_static/releases/test/  # Ensure only admin has write privileges
 echo "Hello World! I'm Web Static!" | sudo tee -a /data/web_static/releases/test/index.html  # Write something to file for testing sake. P.S: The no of Hello World strings in the pseudo webpage is the amount of tines this script was ran.
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current  # -f to remove existing sym link. /current is the sym link referencing /test/ folder
+sudo ln -sf -t /data/web_static/releases/test/ /data/web_static/current  # -f to remove existing sym link. /current is the sym link referencing /test/ folder. Check ln -t [DIR] [DEST] in "man ls"
 sudo chown -R "ubuntu:ubuntu" /data/  # Give file and group ownership to user ubuntu
 
 
 # Edit server block to serve static web content in location /hbnb_static
-config_file="/etc/nginx/sites-enabled/$HOSTMANE.conf"
-# sudo -c "$config_file"  # In case config file hadnt bern created
-sudo sed -i "s/location \/ {/\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n\n\tlocation \/ {/g"  "$config_file"
-# single quotes can be used to prevent any unforseen shell interference on sed. Always ensure the root and alis direcrives ebd with a / followed by a semicolon.
-# an alias was used here mainly for security purposes inorder to obsure the underlying architecture of this page/resource, rather than usubg a root directive which is more revealing of the specific location where the resource is stored.
+config_file="/etc/nginx/sites-available/$HOSTNAME.conf"
+sudo sed -i "s/location \/ {/\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t}\n\n\tlocation \/ {/g"  "$config_file"
+# single quotes can be used to prevent any unforseen shell interference on sed. Always ensure the root and alis direcrives and with a / followed by a semicolon.
+# an alias was used here mainly for security purposes inorder to obsure the underlying architecture of this page/resource, rather than usingg a root directive which is more revealing of the specific location where the resource is stored.
 
 
 # Activate server block if not already previously activated
-if ![ -L "/etc/nginx/sites-enabled" ]  # Does sym link exists here
+if [ ! -L "/etc/nginx/sites-enabled" ]  # Is this dir a sym link
 then
-    sudo ln -s "$config_file" /etc/nginx/sites-enabled/
+	sudo ln -sf "$config_file" /etc/nginx/sites-enabled/
 fi
 
 # Reload/Start Nginx to apply changes
