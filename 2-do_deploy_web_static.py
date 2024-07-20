0147f4cc339c83927df20cbce9163ee0c7eb8e8f
Yermehyaw <yermehyaw@gmail.com>
2024-07-20 16:54:54 -0700

    Fox code logic

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,25 +28,24 @@ def do_deploy(archive_path):
     Return:
     True, if all operations are completed successfully, otherwise, False
     """
-    if exists(archive_path):  # Checks if file dosent exists at archive_path
+    if not exists(archive_path):  # Checks if file dosent exists at archive_path
         return False
-    # Retrieve only archive name without .tgz extension
-    name_with_ext = basename(archive_path)  #remove path 
+    name_with_ext = basename(archive_path)  # retrieve only archive name with .tgz extension
     archive_name = splitext(name_with_ext)[0]  # remove file extension from file name
     env.hosts = ["54.157.166.142", "18.209.178.215"]  # IP to exec commands in
     env.user = "ubuntu"  # default user, if none is entered via fab -u
 
     # Fabric commands
     sudo("mkdir -p /tmp/")  # create target dir if it dosent exist
-    put(f"mv -f {archive_path} /tmp/")  # works without sudo.
+    put(archive_path, "/tmp/")  # works without sudo.
     sudo(f"mkdir -p /data/web_static/releases/{archive_name}/")
-    sudo(f"tar -xzf /tmp/{archive_path} /data/web_static/releases/{archive_name}/")
-    sudo(f"rm -rf /tmp/{archive_name}/")   # delete archive from server
+    sudo(f"tar -xzf /tmp/{name_with_ext} -C /data/web_static/releases/{archive_name}/")
+    sudo(f"rm -rf /tmp/{name_with_ext}/")   # delete archive from server
     sudo(f"rm -f /data/web_static/current")  # delete previous sum link at  /current
     sudo(f"touch /data/web_static/current")  # recreate /current file for sym link
     sudo("ln -sf /data/web_static/releases/{archive_name}/ /data/web_static/current")  # link archive folder to /current file
 
-    end = local("echo $?", capture=True)  # Check the exit status after running
+    end = run("echo $?", capture=True)  # Check the exit status after running
     if end.stdout == 0:
         return True
     else:
