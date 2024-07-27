cf10bc70a05376e47c8bfec0cce33f8fa9912b56
Akor Jeremiah <136399371+Yermehyaw@users.noreply.github.com>
2024-07-27 18:19:16 +0100

    update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,17 +23,12 @@ sudo chown -R "ubuntu:ubuntu" /data/  # Give file and group ownership to user ub
 
 
 # Edit server block to serve static web content in location /hbnb_static
-config_file="/etc/nginx/sites-available/$HOSTNAME.conf"
-sudo sed -i "s/location \/ {/\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t}\n\n\tlocation \/ {/g"  "$config_file"
+config_file="/etc/nginx/sites-available/default.conf"
+sudo sed -i "s/location \/ {/\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tindex index.html;\n\t}\n\n\tlocation \/ {/g"  "$config_file"
 # single quotes can be used to prevent any unforseen shell interference on sed. Always ensure the root and alis direcrives and with a / followed by a semicolon.
 # an alias was used here mainly for security purposes inorder to obsure the underlying architecture of this page/resource, rather than usingg a root directive which is more revealing of the specific location where the resource is stored.
 
 
-# Activate server block if not already previously activated
-if [ ! -L "/etc/nginx/sites-enabled" ]  # Is this dir a sym link
-then
-	sudo ln -sf "$config_file" /etc/nginx/sites-enabled/
-fi
 
 # Reload/Start Nginx to apply changes
 sudo nginx -s reload
